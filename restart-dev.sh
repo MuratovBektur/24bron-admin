b95ad2e11d8ec1b9f85cc8db69e17bccd52b438e
Bektur Muratov <prostreyd@gmail.com>
2026-03-28 22:31:33 +0600

    first commit

---
+++ restart-dev.sh
@@ -0,0 +1,12 @@
+git pull;
+
+echo "🏗️  Сборка frontend..."
+cd client && ./build.sh && cd ..
+
+# Получаем имя docker compose команды
+# (либо docker compose либо docker-compose)
+source docker-compose-name.sh;
+
+"${DOCKER_COMPOSE[@]}" -f docker-compose.dev.yml down -v;
+"${DOCKER_COMPOSE[@]}" -f docker-compose.dev.yml up -d --build;
+"${DOCKER_COMPOSE[@]}" -f docker-compose.dev.yml logs -f --tail=100;
---
+++ restart.sh
@@ -0,0 +1,13 @@
+git pull;
+
+echo "🏗️  Сборка frontend..."
+cd client && ./build.sh && cd ..
+
+# получем имя докер композера 
+# (либо docker compose либо docker-compose)
+# по которому можно обращяться
+source docker-compose-name.sh;
+
+"${DOCKER_COMPOSE[@]}" down -v;
+"${DOCKER_COMPOSE[@]}" up -d --build;
+"${DOCKER_COMPOSE[@]}" logs -f --tail=100;
